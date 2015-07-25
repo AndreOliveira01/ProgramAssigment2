439f0866140404f8552b9088ebe77d6e82929c83
André Oliveira <AndreOliveira01@users.noreply.github.com>
2015-07-25 18:03:03 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,1 +1,40 @@
+# R Programming Course
+# Programming Assignment 2: Lexical Scoping
 
+makeCacheMatrix <- function(x = matrix()) {
+        xinv <- NULL 
+        set <- function(y) {
+                x <<- y
+                xinv <<- NULL 
+        }
+        
+        get <- function() x 
+        setInv <- function(inv) xinv <<- inv 
+        getInv <- function() xinv
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
+}
+
+cacheSolve <- function(x, ...) {
+        m <- x$getInv()
+        if(!is.null(m)) { 
+                message("get cache data")
+                return(m) 
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setInv(m)
+        m
+}
+
+# Testing
+test <- matrix(runif(9,1,100),3,3)
+testCached <- makeCacheMatrix(test)
+
+
+testInv <- cacheSolve(testCached)
+testInv <- cacheSolve(testCached)
+testInv <- cacheSolve(testCached)
+testInv <- cacheSolve(testCached)
+testInv <- cacheSolve(testCached)
